4f4c0ffcf2dc044a28d67ee2c4b7b9a2ff27f093
aelabouye <154388201+aelabouye@users.noreply.github.com>
2023-12-21 14:13:51 +0100

    update

--- achbine/Form1.Designer.cs
+++ achbine/Form1.Designer.cs
@@ -31,6 +31,11 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.start = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
@@ -40,16 +45,11 @@
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.button2 = new System.Windows.Forms.Button();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -83,7 +83,7 @@
             | System.Windows.Forms.AnchorStyles.Left)));
             this.groupBox1.Controls.Add(this.progressBar1);
             this.groupBox1.Controls.Add(this.groupBox3);
-            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.start);
             this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Controls.Add(this.richTextBox1);
@@ -94,6 +94,57 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Work";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 439);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(426, 47);
+            this.progressBar1.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton3);
+            this.groupBox3.Controls.Add(this.radioButton4);
+            this.groupBox3.Location = new System.Drawing.Point(107, 318);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(205, 31);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Malgun Gothic", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(93, 9);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(68, 21);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.Text = "Extract";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Font = new System.Drawing.Font("Malgun Gothic", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton4.Location = new System.Drawing.Point(6, 10);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(68, 21);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Remov";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(16, 400);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(426, 32);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start Extarct";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -176,57 +227,6 @@
             this.openFileDialog1.Filter = "(*.txt)|*.txt";
             this.openFileDialog1.Multiselect = true;
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(16, 400);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(426, 32);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Start Extarct";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.radioButton3);
-            this.groupBox3.Controls.Add(this.radioButton4);
-            this.groupBox3.Location = new System.Drawing.Point(107, 318);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(205, 31);
-            this.groupBox3.TabIndex = 3;
-            this.groupBox3.TabStop = false;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Font = new System.Drawing.Font("Malgun Gothic", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton3.Location = new System.Drawing.Point(93, 9);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(68, 21);
-            this.radioButton3.TabIndex = 1;
-            this.radioButton3.Text = "Extract";
-            this.radioButton3.UseVisualStyleBackColor = true;
-            // 
-            // radioButton4
-            // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Checked = true;
-            this.radioButton4.Font = new System.Drawing.Font("Malgun Gothic", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.radioButton4.Location = new System.Drawing.Point(6, 10);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(68, 21);
-            this.radioButton4.TabIndex = 0;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Remov";
-            this.radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Location = new System.Drawing.Point(16, 439);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(426, 47);
-            this.progressBar1.TabIndex = 5;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -239,10 +239,10 @@
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -263,7 +263,7 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.RadioButton radioButton3;
         private System.Windows.Forms.RadioButton radioButton4;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button start;
         private System.Windows.Forms.ProgressBar progressBar1;
     }
 }
--- achbine/Form1.cs
+++ achbine/Form1.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.Json;
 
 namespace achbine
 {
@@ -15,6 +18,35 @@ namespace achbine
         public Form1()
         {
             InitializeComponent();
+            LoadExcludedISPs();
+        }
+        private void LoadExcludedISPs()
+        {
+            try
+            {
+                // Read the JSON content from the file
+                string jsonContent = File.ReadAllText("provide.json");
+
+                // Deserialize the JSON into a class
+                ProvidersData providersData = JsonSerializer.Deserialize<ProvidersData>(jsonContent);
+
+                // Get the excluded ISPs from the class
+                excludedISPs = providersData?.Exclude ?? new List<string>();
+
+                // Populate the ComboBox with excluded ISPs
+                comboBox1.Items.AddRange(excludedISPs.ToArray());
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions (e.g., file not found, JSON parsing error)
+                MessageBox.Show($"Error loading excluded ISPs: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Class representing the data structure in provide.json
+        public class ProvidersData
+        {
+            public List<string> Exclude { get; set; }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -55,12 +87,59 @@ namespace achbine
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            openFileDialog1.ShowDialog();
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // You can handle the selected files here if needed
+                MessageBox.Show("File Uploude successfully.");
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            progressBar1.Value = 100;
+            progressBar1.Value = 0; // Reset progress bar
+            progressBar1.Maximum = openFileDialog1.FileNames.Length; // Set maximum value for progress bar
+
+            // Move the outputFile declaration outside the loop
+            string outputFile = "output_emails.txt";
+
+            foreach (string filePath in openFileDialog1.FileNames)
+            {
+                // Read each file and extract emails
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    // Use a regular expression to find emails in each line
+                    MatchCollection matches = Regex.Matches(line, @"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b");
+
+                    // Save emails to a text file
+                    foreach (Match match in matches)
+                    {
+                        File.AppendAllText(outputFile, match.Value + Environment.NewLine);
+                    }
+                }
+
+                // Update progress bar
+                progressBar1.Value++;
+            }
+
+            MessageBox.Show("Emails extracted and saved to output_emails.txt successfully.");
+
+            // Open a SaveFileDialog to allow the user to choose where to save the file
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            saveFileDialog.FileName = "output_emails.txt";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Copy the content of the generated file to the selected destination
+                File.Copy(outputFile, saveFileDialog.FileName, true);
+                MessageBox.Show("File downloaded successfully.");
+            }
         }
+
     }
 }
